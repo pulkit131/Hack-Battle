23f7d4cec53afb8fbb278b4838dc1066fdcb6d7f
Pulkit Nandwana <pulkit131nandwana@gmail.com>
2024-09-25 10:54:26 +0530

    Horizontal_home_page

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import HorizontalScroll from "./components/HorizontalScroll";
+function App() {
+  return (
+    <>
+      <HorizontalScroll />
+    </>
+  );
+}
+
+export default App;
