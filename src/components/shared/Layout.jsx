91d67b47b3284c458c0b8b1769176f997413233c
Ayush200704 <ayush.tiwari.200704@gmail.com>
2024-09-26 12:01:24 +0530

    layout change

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Sidebar from './Sidebar'
 import Header from './Header'
 
 export default function Layout() {
 	return (
-		<div className="bg-gray-200 h-screen w-screen overflow-hidden flex flex-row">
-			<Sidebar />
-			<div className="flex flex-col flex-1">
-				<Header />
-				<div className="flex-1 p-4 min-h-0 overflow-auto bg-gray-200">
-					<Outlet />
-				</div>
+		<div className="bg-gray-200 min-h-screen h-full w-full overflow-hidden flex flex-col">
+			{/* Header */}
+			<Header />
+			
+			{/* Main content area */}
+			<div className="flex-1 p-4 min-h-0 overflow-y-auto bg-gray-200">
+				<Outlet />
 			</div>
 		</div>
 	)
